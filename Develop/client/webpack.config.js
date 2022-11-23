c050b57c13059bd652b1e39fd3c05e844952d913
TomLuna <lomtuna15@gmail.com>
2022-11-23 01:33:37 -0600

    updated service worker logic

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,6 +4,8 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+new WorkboxPlugin.GenerateSW()
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
